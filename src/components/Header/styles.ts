34206949d6c3ab77456db7904c0fbbdc2375038e
Raphael Simões de Faria <raphaelsimoes@gmail.com>
2020-12-11 12:27:20 -0300

    Entrega do desafio

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -22,6 +22,8 @@ export const Container = styled.div<ContainerProps>`
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
+        padding-bottom: 10px;
+        border-bottom: 2px solid #ff872c;
 
         & + a {
           margin-left: 32px;
